78ca51d8dca36f2c12abd5e860ce10afc033e96d
simon-kowerski <kowerski8@gmail.com>
2024-03-16 01:11:06 -0400

    Version 1.0.1

---
+++ usr/share/SPICESat-OSImage/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#updates SPICESat-OSImage package
+
+#Creation Date: 3/16/2024
+#Last Updated: 3/16/2024
+#Author: Simon Kowerski :)
+
+#Version: 1.0
+
+sudo wget -O - https://raw.githubusercontent.com/rutgers-star/Payload-OSImager/main/installer | bash
+
